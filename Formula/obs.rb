874585b48fd3f1e6b44726c187fb546da0dcbd36
Chris D <faithoflifedev@gmail.com>
2022-12-01 14:36:10 -0500

    v5.0.0+14

--- Formula/obs.rb
+++ Formula/obs.rb
@@ -1,8 +1,8 @@
 class Obs < Formula
   desc "obs_websocket allows dart-based connections to the OBS (Open Broadcaster Software) plugin obs-websocket"
   homepage "https://github.com/faithoflifedev/easy_obs_websocket"
-  url "https://api.github.com/repos/faithoflifedev/obs_websocket/tarball/v5.0.0+13"
-  sha256 "8d37628cd23499a525170398bbf36c7a395ccdedcc6493a6440e865663af1a6b"
+  url "https://api.github.com/repos/faithoflifedev/obs_websocket/tarball/v5.0.0+14"
+  sha256 "63a601a8f4cb69b375d734f54b0906a0b89714da17b0d73ec028ca55b21f94fc"
   license "MIT"
   
   depends_on "dart-lang/dart/dart" => :build
@@ -14,6 +14,6 @@ class Obs < Formula
   end
   
   test do
-    assert_match "obs_websocket v5.0.0+13", shell_output("bin/obs version")
+    assert_match "obs_websocket v5.0.0+14", shell_output("bin/obs version")
   end
 end
